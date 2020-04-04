043ff95e2bbeb6ec78a3ec3d6b81bb6eedbca263
meghna-a <meghna@agnish.com>
2020-04-04 00:46:42 -0400

    Update logic..js

--- Map2/logic..js
+++ Map2/logic..js
@@ -2,7 +2,6 @@ var myMap = L.map("map", {
   center: [38, -97],
   zoom: 5
 });
-
 // Adding a tile layer (the background map image) to our map
 // We use the addTo method to add objects to our map
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -11,67 +10,45 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   id: "mapbox.streets-basic",
   accessToken: API_KEY
 }).addTo(myMap);
-
-var cities = [
-  {
-    name: "New York",
-    location: [40.663468,-73.938697],
-    totalcases: 20875,
-    totaldeaths: 157,
-    date: 032020
-  },
-  {
-    name: "New Jersey",
-    location: [40.72422,-74.172574],
-    totalcases: 2844,
-    totaldeaths: 27
-  },
-  {
-    name: "Washington",
-    location: [47.619349,-122.351471],
-    totalcases: 2221,
-    totaldeaths: 110
-  },
-  {
-    name: "California",
-    location: [34.019394,-118.410825],
-    totalcases: 2133,
-    totaldeaths: 40
-  },
-  {
-    name: "Michigan",
-    location: [42.383037,-83.102237],
-    totalcases: 1328,
-    totaldeaths: 15
-  }
-];
-// /Loop through the countries array
+// var formatTime = d3.timeFormat("%B %d, %Y");
+// formatTime("March 23, 2020");
+// var date = "March 23, 2020"
+var lat = [];
+var lng = [];
+// var data = d3.csvParse("latlng.csv")
+d3.csv("latlng.csv", function(data){
+  console.log(data)
+  // console.log("test")
+var cities = data;
+// if (error) throw error;
 for (var i = 0; i < cities.length; i++){
-
-// Conditionals for countries points
-var color = "";
-if (cities[i].totaldeaths > 150) {
-  color = "yellow";
+  cities[i].Lat = +cities[i].Lat;
+  cities[i].Lng = +cities[i].Lng;
+  cities[i].TotalDeaths = +cities[i].TotalDeaths;
+  // Conditionals for cities points
+  function getColor (state){
+     if (state > 150) {
+        color = "yellow";
+      }
+      else if (state > 50) {
+        color = "blue";
+      }
+      else if (state < 20 ){
+        color = "green";
+      }
+      else {
+        color = "red";
+      }
+      return color};
 }
-else if (cities[i].totaldeaths > 50) {
-  color = "blue";
-}
-else if (cities[i].totaldeaths < 20 ){
-  color = "green";
-}
-else {
-  color = "red";
-}
-  // Add circles to map
-  L.circle(cities[i].location, {
-    fillOpacity: 0.75,
-    color: "white",
-    fillColor: "red",
-    // Setting our circle's radius equal to the output of our markerSize function
-    // This will make our marker's size proportionate to its population
-    radius: cities[i].totaldeaths * 1500
-  }).bindPopup("<h1>" + cities[i].name + "<h1> <hr> <h2> Total Cases: " + cities[i].totalcases + "</h2> <hr> <h3> Total Deaths: " + cities[i].totaldeaths + 
-  "</h3> <hr> <h4> Date: " + cities[i].date + "</h4>"  ).addTo(myMap);
-}
-
-  // Adjust radius
+ // Add circles to map
+for (var i = 0; i < cities.length; i++){
+    L.circleMarker([cities[i].Lat, cities[i].Lng], {
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: getColor(cities[i].TotalDeaths),
+      radius: cities[i].TotalDeaths 
+    }).bindPopup("<h1>" + cities[i].USAState + "<h1> <hr> <h2> Total Cases: " + cities[i].TotalCases + "</h2> <hr> <h3> Total Deaths: " + cities[i].TotalDeaths + 
+    "</h3> <hr> <h4> Date: March 23,2020" + "</h4>"  ).addTo(myMap);
+  }
+})
